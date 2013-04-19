d467423d13d2b499654e0e2db1ad0317dc9f99dd
Marco Scheidhuber <j23d@jusid.de>
2013-04-19 11:57:58 +0200

    After the tiles are saved, the page will be reloaded.

--- kotti_grid/views.py
+++ kotti_grid/views.py
@@ -7,12 +7,16 @@ from kotti.interfaces import IContent
 from kotti_grid.fanstatic import kotti_grid
 from kotti_grid.utils import save_grid_settings
 
+from kotti_grid import _
+
 
 @view_config(name="save-grid", permission="edit",
              request_method="POST", renderer="json")
 def save_grid(context, request):
     data = request.POST
     tiles = eval(data['tiles'])
+    request.session.flash(
+                _(u'The tiles have been saved.'), 'success')
     return save_grid_settings(tiles, 'tiles')
 
 
