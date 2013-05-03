d5d3beed8cabdb7c6b9af37cb2dd3ee9409bf43b
Marco Scheidhuber <j23d@jusid.de>
2013-05-03 11:14:34 +0200

    Move doctest to functional.

--- kotti_grid/tests/conftest.py
+++ kotti_grid/tests/conftest.py
@@ -7,3 +7,47 @@ pytest_plugins = "kotti"
 def kg_populate(db_session):
     from kotti_grid.populate import populate
     populate()
+
+
+def settings():
+    from kotti import _resolve_dotted
+    from kotti import conf_defaults
+    settings = conf_defaults.copy()
+    settings['kotti.secret'] = 'secret'
+    settings['kotti.secret2'] = 'secret2'
+    settings['kotti.configurators'] = \
+        'kotti_grid.kotti_configure kotti_settings.kotti_configure'
+    settings['kotti.populators'] = 'kotti.testing._populator'
+    _resolve_dotted(settings)
+    return settings
+
+
+def setup_app():
+    from kotti import base_configure
+    return base_configure({}, **settings()).make_wsgi_app()
+
+
+@fixture
+def kg_browser(db_session, request):
+    """ returns an instance of `zope.testbrowser`.  The `kotti.testing.user`
+        pytest marker (or `pytest.mark.user`) can be used to pre-authenticate
+        the browser with the given login name: `@user('admin')`.
+    """
+    from wsgi_intercept import add_wsgi_intercept, zope_testbrowser
+    from kotti.testing import BASE_URL
+    host, port = BASE_URL.split(':')[-2:]
+    add_wsgi_intercept(host[2:], int(port), setup_app)
+    browser = zope_testbrowser.WSGI_Browser(BASE_URL + '/')
+    if 'user' in request.keywords:
+        # set auth cookie directly on the browser instance...
+        from pyramid.security import remember
+        from pyramid.testing import DummyRequest
+        login = request.keywords['user'].args[0]
+        environ = dict(HTTP_HOST=host[2:])
+        for _, value in remember(DummyRequest(environ=environ), login):
+            cookie, _ = value.split(';', 1)
+            name, value = cookie.split('=')
+            if name in browser.cookies:
+                del browser.cookies[name]
+            browser.cookies.create(name, value.strip('"'), path='/')
+    return browser
---
+++ kotti_grid/tests/test_functional.py
@@ -0,0 +1,29 @@
+from kotti.testing import user
+from kotti.testing import BASE_URL
+
+
+@user('admin')
+def test_grid_widget(kg_browser, kg_populate):
+    kg_browser.open(BASE_URL)
+    assert '<input type="hidden" id="grid-fieldname"' in kg_browser.contents
+    assert '<div class="gridster">' in kg_browser.contents
+    assert '<button id="save-tiles" class="btn">Save tiles</button>' \
+        in kg_browser.contents
+    assert '<span id="add-tile" class="tile-action icon-plus">' \
+        in kg_browser.contents
+
+
+@user('admin')
+def test_grid_settings(kg_browser, kg_populate):
+    ctlr = kg_browser.getControl
+    kg_browser.open(BASE_URL + '/@@settings')
+
+    assert 'name="__formid__" value="kotti_grid"' \
+        in kg_browser.contents
+    assert '<span>Settings for kotti_grid</span>' in kg_browser.contents
+    assert 'Height' in kg_browser.contents
+    assert 'name="kotti_grid-height" value="150"' in kg_browser.contents
+
+    ctlr(name='kotti_grid-height', index=0).value = '300'
+    ctlr(name='save', index=0).click()
+    assert 'name="kotti_grid-height" value="300"' in kg_browser.contents
