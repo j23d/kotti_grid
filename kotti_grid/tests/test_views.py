3fc6c6edf852538d51dec10760b8faa26dd3e0d0
Marco Scheidhuber <j23d@jusid.de>
2013-05-14 17:31:52 +0200

    Test grid_setting view.

--- kotti_grid/tests/test_views.py
+++ kotti_grid/tests/test_views.py
@@ -1,7 +1,15 @@
 
 
-def test_gridbrowser(db_session, dummy_request, root):
+def test_gridbrowser_view(db_session, dummy_request, root):
     from kotti_grid.views import GridBrowser
     gt = GridBrowser(root, dummy_request)
     browser = gt.gridbrowser()
     assert browser['form'].startswith('<form')
+
+
+def test_grid_settings_view(db_session, dummy_request, root):
+    from kotti_grid.views import grid_settings
+    view = grid_settings(root, dummy_request)
+    assert view == {'width': 150, 'margin_y': 10,
+                    'margin_x': 10, 'resize_tiles': False,
+                    'height': 150}
